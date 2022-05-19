bd174977819ddcd9d7642e52d4df9b6295cdf34f
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2022-05-19 18:33:13 -0300

    Finalizando a parte referente ao banco de dados

---
+++ database/migrations/2022_05_19_210050_create_tarefas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',120);
+            $table->date('dt_inicio');
+            $table->date('dt_termino');
+            $table->integer('status')->default(0);
+            $table->unsignedBigInteger('categoria_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
